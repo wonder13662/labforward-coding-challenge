071deee5a6bb6738a0a51eae4aa1e7036b30eebc
Wonder Jung <wonder13662@gmail.com>
2019-10-28 23:04:47 +0900

    Add graph

--- src/components/series-data-list/SeriesDataListItem.js
+++ src/components/series-data-list/SeriesDataListItem.js
@@ -1,25 +1,105 @@
 import React from "react";
-import ActualDataGraph from "../actual-data-graph";
-import AreaOfInterestGraph from "../area-of-interest-graph";
+import { ResponsiveLine } from "@nivo/line";
 
 class SeriesDataListItem extends React.Component {
-  render() {
-    const { actualData, areaOfInterest } = this.props;
-
+  getLineChart({
+    data,
+    enableArea = false,
+    legendAxisLeft,
+    legendAxisBottom,
+    color
+  }) {
     return (
-      <li>
-        <div className="item-box">
-          <div className="item-box--actual-data-graph">
-            <ActualDataGraph actualData={actualData}></ActualDataGraph>
-          </div>
-          <div className="item-box--area-of-intereset-graph">
-            <AreaOfInterestGraph
-              areaOfInterest={areaOfInterest}
-            ></AreaOfInterestGraph>
-          </div>
-        </div>
-      </li>
+      <ResponsiveLine
+        data={data}
+        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        xScale={{ type: "point" }}
+        yScale={{ type: "linear", stacked: true, min: "auto", max: "auto" }}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          orient: "bottom",
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: legendAxisBottom,
+          legendOffset: 36,
+          legendPosition: "middle"
+        }}
+        axisLeft={{
+          orient: "left",
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: legendAxisLeft,
+          legendOffset: -40,
+          legendPosition: "middle"
+        }}
+        colors={[color]}
+        borderColor={{ from: "color" }}
+        enableArea={enableArea}
+        animate={false}
+        pointSize={8}
+        pointColor="#ffffff"
+        pointBorderWidth={2}
+        pointBorderColor={{ from: "serieColor" }}
+        pointLabel="y"
+        pointLabelYOffset={-12}
+        useMesh={true}
+        legends={[
+          {
+            anchor: "bottom-right",
+            direction: "column",
+            justify: false,
+            translateX: 100,
+            translateY: 0,
+            itemsSpacing: 0,
+            itemDirection: "left-to-right",
+            itemWidth: 80,
+            itemHeight: 20,
+            itemOpacity: 0.75,
+            symbolSize: 12,
+            symbolShape: "circle",
+            symbolBorderColor: "rgba(0, 0, 0, .5)",
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemBackground: "rgba(0, 0, 0, .03)",
+                  itemOpacity: 1
+                }
+              }
+            ]
+          }
+        ]}
+      />
     );
+  }
+
+  render() {
+    const {
+      data,
+      name,
+      color,
+      enableArea,
+      legendAxisLeft,
+      legendAxisBottom
+    } = this.props;
+
+    const graphData = {
+      id: name,
+      data: data.map((value, idx) => ({ x: `${idx}`, y: `${value}` }))
+    };
+
+    const lineChart = this.getLineChart({
+      data: [graphData],
+      enableArea,
+      legendAxisLeft,
+      legendAxisBottom,
+      color
+    });
+
+    return <li className="item-box">{lineChart}</li>;
   }
 }
 
--- src/components/series-data-list/index.js
+++ src/components/series-data-list/index.js
@@ -2,19 +2,50 @@ import React from "react";
 import SeriesDataListItem from "./SeriesDataListItem";
 import "../../css/serice-data-set.css";
 
+const COLORS = ["#1f78b4", "#e31a1c", "#f1e15b"];
+
 class SeriesDataList extends React.Component {
   render() {
     const { actualDataList, areaOfInterestList } = this.props;
 
-    const listItems = actualDataList.map((actualData, idx) => {
-      return (
+    const listItems = [];
+    for (let i = 0; i < actualDataList.length; i++) {
+      listItems.push(
+        <li>
+          <h3>{`Series-${i + 1}`}</h3>
+        </li>
+      );
+
+      const actualData = actualDataList[i];
+      let key = `actualData-${i + 1}`;
+      listItems.push(
         <SeriesDataListItem
-          key={idx}
-          actualData={actualData}
-          areaOfInterest={areaOfInterestList[idx]}
+          key={key}
+          color={COLORS[i]}
+          name={key}
+          data={actualData}
+          enableArea={false}
+          legendAxisLeft={"Data"}
+          legendAxisBottom={"Time Series"}
         />
       );
-    });
+
+      const areaOfInterest = areaOfInterestList[i];
+      key = `areaOfInterest-${i + 1}`;
+      listItems.push(
+        <SeriesDataListItem
+          key={key}
+          color={COLORS[i]}
+          name={key}
+          data={areaOfInterest}
+          enableArea={true}
+          legendAxisLeft={"Signal"}
+          legendAxisBottom={"Time Series"}
+        />
+      );
+    }
+
+    console.log("listItems.length:", listItems.length);
 
     return (
       <div className="series-data-list-box">
