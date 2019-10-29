18662102bbc5c4da5e2c0ec2b262792f94167358
Wonder Jung <wonder13662@gmail.com>
2019-10-29 22:03:29 +0900

    Add mock API:Errors - Database, Unknown

--- src/api/fake-api.js
+++ src/api/fake-api.js
@@ -1,15 +1,47 @@
 import actualData from "../mockup/actual-data";
 import areaOfInterest from "../mockup/area-of-interest";
+import { DatabaseError } from "../error";
 
-export default {
-  fetchActualDataList: function() {
-    return new Promise((resolve, reject) => {
-      resolve({ data: actualData });
-    });
-  },
-  fetchAreaOfInterestList: function() {
-    return new Promise((resolve, reject) => {
-      resolve({ data: areaOfInterest });
-    });
-  }
+const fetchActualDataList = function() {
+  return new Promise((resolve, reject) => {
+    resolve({ data: actualData });
+  });
+};
+
+const fetchAreaOfInterestList = function() {
+  return new Promise((resolve, reject) => {
+    resolve({ data: areaOfInterest });
+  });
+};
+
+const fetchActualDataListUnknowError = function() {
+  return new Promise((resolve, reject) => {
+    resolve({ data: actualData });
+  });
+};
+const fetchAreaOfInterestListUnknownError = function() {
+  return new Promise((resolve, reject) => {
+    resolve({ data: areaOfInterest });
+  });
+};
+
+const fetchActualDataListDatabaseError = function() {
+  return new Promise((resolve, reject) => {
+    reject(new DatabaseError("Fail to fetch the data from server!"));
+  });
+};
+
+const fetchAreaOfInterestListDatabaseError = function() {
+  return new Promise((resolve, reject) => {
+    reject(new DatabaseError("Fail to fetch the data from server!"));
+  });
+};
+
+export {
+  fetchActualDataList,
+  fetchAreaOfInterestList,
+  fetchActualDataListUnknowError,
+  fetchAreaOfInterestListUnknownError,
+  fetchActualDataListDatabaseError,
+  fetchAreaOfInterestListDatabaseError
 };
